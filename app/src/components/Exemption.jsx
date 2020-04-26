095952ce5a4f9d201a75d68539ac8e89ae703a0c
yelkar-snehal <yelkarsnehal9@gmail.com>
2020-04-26 20:51:54 +0530

    Modify some textfields type to select

--- app/src/components/Exemption.jsx
+++ app/src/components/Exemption.jsx
@@ -27,19 +27,43 @@ const useStyles = makeStyles(theme => ({
 export const Exemption = (props) => {
     const classes = useStyles();
 
-    const [city, setCity] = useState("");
+    const [city, setCity] = useState("Mumbai");
     const [rent, setRent] = useState("");
     const [basicSalary, setBasicSalary] = useState("");
+    const [da, setDA] = useState("");
     const [hraReceived, setHRAReceived] = useState("");
+
+    const cities = [
+        {
+            value: 'Chennai',
+            label: 'Chennai'
+        },
+        {
+            value: 'Delhi',
+            label: 'Delhi'
+        },
+        {
+            value: 'Kolkata',
+            label: 'Kolkata'
+        },
+        {
+            value: 'Mumbai',
+            label: 'Mumbai'
+        },
+        {
+            value: 'other',
+            label: 'Other Non-metropolitian Cities'
+        }
+    ];
 
     return (
         <Fragment>
             <CssBaseline />
-            <Container maxWidth="md">
+            <Container maxWidth="sm">
                 <div className={classes.paper}>
                     <form className={classes.form}>
                         <Grid container spacing={3}>
-                            <Grid item xs={12} sm={6}>
+                            <Grid item xs={12}>
                                 <TextField
                                     name="city"
                                     id="city"
@@ -49,10 +73,21 @@ export const Exemption = (props) => {
                                     required
                                     autoComplete="city"
                                     autoFocus
+                                    select
+                                    SelectProps={{
+                                        native: true,
+                                    }}
                                     value={city}
-                                    onChange={(event) => setCity(event.target.value)} />
+                                    helperText="Calculation of income tax will differ for metro and non-metro cities."
+                                    onChange={(event) => setCity(event.target.value)}>
+                                    {cities.map((option) => (
+                                        <option key={option.value} value={option.value}>
+                                            {option.label}
+                                        </option>
+                                    ))}
+                                </TextField>
                             </Grid>
-                            <Grid item xs={12} sm={6}>
+                            <Grid item xs={12}>
                                 <TextField
                                     name="basicSalary"
                                     id="basicSalary"
@@ -64,19 +99,31 @@ export const Exemption = (props) => {
                                     value={basicSalary}
                                     onChange={(event) => setBasicSalary(event.target.value)} />
                             </Grid>
-                            <Grid item xs={12} sm={6}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    name="da"
+                                    id="da"
+                                    label="Dearness Allowances"
+                                    variant="outlined"
+                                    fullWidth
+                                    autoComplete="da"
+                                    value={da}
+                                    helperText="Dearness Allowance (DA) is a calculation on inflation and allowance paid to government employees, public sector employees (PSE) and pensioners."
+                                    onChange={(event) => setDA(event.target.value)} />
+                            </Grid>
+                            <Grid item xs={12}>
                                 <TextField
                                     name="rent"
                                     id="rent"
                                     label="Rent Paid"
                                     variant="outlined"
                                     fullWidth
-                                    required
                                     autoComplete="rent"
                                     value={rent}
+                                    helperText="Annual rent paid by you."
                                     onChange={(event) => setRent(event.target.value)} />
                             </Grid>
-                            <Grid item xs={12} sm={6}>
+                            <Grid item xs={12}>
                                 <TextField
                                     name="hraReceived"
                                     id="hraReceived"
@@ -86,6 +133,7 @@ export const Exemption = (props) => {
                                     required
                                     autoComplete="hraReceived"
                                     value={hraReceived}
+                                    helperText="House Rent Allowance is a part of the salary provided by an employer to his employee for his rented accommodation."
                                     onChange={(event) => setHRAReceived(event.target.value)} />
                             </Grid>
                         </Grid>
